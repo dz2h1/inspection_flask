7b973da533c4a37c40e8a78004bdb9590fa22360
dz2h1 <lizhi0810@outlook.com>
2020-03-13 20:17:55 +0800

    V0.2.1b2

--- config/settings_example.py
+++ config/settings_example.py
@@ -7,16 +7,16 @@ import platform
 ver = 'V0.2.1b2'  # 网页右上角的版本显示
 mongodb_add = '192.168.1.1'  # mongodb数据库地址
 mongodb_port = 27017  # 数据库端口，默认27017
-logs_find_limit = 30  # log页面最大展示条目数量
+logs_find_limit = 8  # log页面最大展示条目数量
 charts_dis_num_limit = 12  # chart页面每个图表数据历史数量
 pageSizeChangeUpper = 105  # size页面变化正常上限，单位（%）
 pageSizeChangelower = 95  # size页面变化正常下线，单位（%），超过上下限将触发报警
 reportSetTime = '0830'  # 巡检邮件报告时间
-python_path = '/usr/bin/python3'  # python命令的地址，用于报警脚本执行
+python_path = 'python'  # python命令的地址，用于报警脚本执行
 sendMail_fileName = '/check_send_mail.py'  # 报警脚本名称
 '''
-v0.1.2版本中支持企业微信消息报警
-在check_base中选择注释from crontab_mail.XXX_settings启用功能
+企业微信消息报警功能开启：
+在check_base中选择注释from crontab_mail.XXX_settings
 '''
 
 
